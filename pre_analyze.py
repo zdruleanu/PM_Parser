94188d0afa94e42d78bc4da76ed53330bdbf0148
Zdruleanu <cris2001_roro@yahoo.com>
2019-02-14 22:38:34 +0100

    initialization based on template. TODO: legend size adjustment

--- pre_analyze.py
+++ pre_analyze.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import shutil
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from pptx import Presentation
@@ -8,7 +9,8 @@ from pptx.util import Pt
 import itertools
 import yaml
 
-class graficCollection:
+
+class GraficCollection:
     '''
     This class is used to contain the data from one single graph(figure)
     '''
@@ -16,7 +18,7 @@ class graficCollection:
         self.figura = figura
         self.grafice = {}
 
-    def extrage_gafice(self, ExcelFile, FilterParams, TargetColumns, UseTargetColumnsInLegend):
+    def extrage_gafice(self, excel_file, filter_params, target_columns, use_target_columns_in_legend):
         # The function returns a list of pandas series, where each series will be ploted on the same plot
         # ExcelFile is the imported DataFrame (already indexed by time)
         # FilterParams is a dictionary with keys being column names and values being a tuple, where the first item is
@@ -26,66 +28,114 @@ class graficCollection:
         #
         # FilterParams = [['Slot', '==',[0,1,4]], ['altceva', '==',['d','d']], ['al3', '<=',[7]]]
 
-
-        FilterParamNumber = FilterParams.__len__()
-        AllColValuesLists = [i[2] for i in FilterParams]
+        FilterParamNumber = filter_params.__len__()
+        AllColValuesLists = [i[2] for i in filter_params]
         AllColValuesListsCombinations = list(itertools.product(*AllColValuesLists))
 
         while True:
             try:
-                comb = AllColValuesListsCombinations.pop(0)
+                comb = list(AllColValuesListsCombinations.pop(0))
             except:
                 print('Finished graphs data generation')
                 break
             FilterExpression = '('
             for i in range(0,FilterParamNumber):
-                FilterExpression += "(ExcelFile['" + FilterParams[i][0] + "'] " + FilterParams[i][1] + " " + str(comb[i]) + ")"
+                print(comb[i])
+                if isinstance(comb[i], str):
+                    comb[i] = "'"+comb[i]+"'"
+                FilterExpression += "(excel_file['" + filter_params[i][0] + "'] " + filter_params[i][1] + \
+                                    " " + str(comb[i]) + ")"
                 if i < FilterParamNumber-1:
                     FilterExpression += " & "
                 else:
                     FilterExpression += ")"
             print("FiltereExpression is: ", FilterExpression)
-            filterResult = ExcelFile[eval(FilterExpression)]
+            filterResult = excel_file[eval(FilterExpression)]
             PrefixNumeGrafic = ''
-            # for FilterParam in comb:
-            #    PrefixNumeGrafic += str(FilterParam) + "_"
-            # we consider the first filter parameter relevnat for the name of the grapsh as it will be shown in the legend. For example it will represent the Slot number
-            PrefixNumeGrafic = str(comb[0])
+            for FilterParam in comb:
+                PrefixNumeGrafic += str(FilterParam) + "_"
+            # We consider the first filter parameter relevnat for the name of the grapsh as it will be shown in the 
+            # legend. For example it will represent the Slot number
+            #PrefixNumeGrafic = str(comb[0])
 
-            for TargetColumn in TargetColumns:
+            for TargetColumn in target_columns:
                 if not filterResult[TargetColumn].empty:
-                    if UseTargetColumnsInLegend:
+                    if use_target_columns_in_legend:
                         self.grafice[PrefixNumeGrafic + "_" + TargetColumn] = (filterResult[TargetColumn])
                     else:
                         # Filtering parameters might lead to a result where the PrefixNumeGrafic is not unique.
                         # If so, exit and print Error
                         if PrefixNumeGrafic in self.grafice.keys():
-                            print("ERROR: graph names are not unique. Please set UseTargetColumnsInLegend to True in the config file")
+                            print("ERROR: graph names are not unique. Please set UseTargetColumnsInLegend "
+                                  "to True in the config file")
                             exit()
                         else:
                             self.grafice[PrefixNumeGrafic] = (filterResult[TargetColumn])
 
-def find_csv_filenames(path_to_dir, suffix=""):
-    filenames = os.listdir(path_to_dir)
-    return [path_to_dir+"/"+filename for filename in filenames if filename.endswith(suffix)]
 
-def initializeConfigFiles(path_to_dir):
-    filenames = find_csv_filenames("./data")
+def find_data_filenames(path_to_dir, prefix="", suffix=""):
+    # returns a list of file names which include prefix and suffix.
+    # the file names returned include the path
+    # directories are not returned
+
+    filenames = [path_to_dir+"/"+filename for filename in os.listdir(path_to_dir) if (filename.startswith(prefix) and
+                 filename.endswith(suffix) and os.path.isfile(path_to_dir+"/"+filename))]
+    return filenames
+
+
+def check_if_template(extracted_for_template_check, template_dir):
+    # Checks in the templateDir if there exists a file whos name begins with extractedForTemplateCheck.
+    # Returns the template file name (with path)
+    print("Checking for tempates")
+    templateFileName = find_data_filenames(template_dir, prefix=extracted_for_template_check)
+    if len(templateFileName) == 0:
+        return False
+    elif len(templateFileName) > 1:
+        print("More than one template matched. Please check the prefix used or the template names")
+    else:
+        return templateFileName
+
+
+def initialize_config_files(path_to_data, path_to_templates):
+    filenames = find_data_filenames(path_to_data)
     for filename in filenames:
         if os.path.splitext(filename)[1] == '.yaml':
             continue
 
+        # extractForTemplateCheck gets the prefix of the base filename (without the path). 
+        # This is to be used to find the relevant tempalte in templates
+        extractForTemplateCheck = os.path.basename(filename).split('_')[0]
+
+        # the configFilename includes the path, the same as filename does
         configFileName = os.path.splitext(filename)[0]+".yaml"
+
         if configFileName in filenames:
             print(filename + " already has the corresponding config file: " + configFileName)
         else:
-            configFile = open(configFileName, "w")
-            configFile.close()
-            print("Created " + configFileName + " config file for " + filename)
+            template = check_if_template(extractForTemplateCheck, path_to_templates)
+            if template == False:
+                configFile = open(configFileName, "w")
+                configFile.close()
+                print("Created " + configFileName + " as empty config file for " + filename)
+            else:
+                shutil.copy(template[0], configFileName)
+                print("Created " + configFileName + " config file from " + template[0] + " template, for " + filename)
     print("Finished config files initialization")
 
-initializeConfigFiles("./data")
-fileNames = find_csv_filenames("./data", '.xls')
+
+def fileToPandas(fileName, fileExtension, skipRows=0):
+    if fileExtension == '.csv':
+        return pd.read_csv(fileName, skiprows=skipRows)
+    else:
+        return pd.read_excel(fileName, skipRows=skipRows)
+
+
+dataPath = "./data/VDF_13.02.19"
+templatesPath = "./templates"
+filesExtension = '.csv'
+initialize_config_files(dataPath, templatesPath)
+fileNames = find_data_filenames(dataPath, suffix=filesExtension)
+skippedRowsNumber = 7
 
 # prepare presentation helpers
 prs = Presentation()
@@ -106,7 +156,13 @@ for fileName in fileNames:
         print(fileName + " has an empty config file")
         continue
 
-    excelfile = pd.read_excel(fileName)
+    #check if 'Result' is in the first line of the file. If so, remove 7 lines
+    line = open(fileName).readline()
+    if 'Result' in line:
+        excelfile = fileToPandas(fileName, filesExtension, skippedRowsNumber)
+    else:
+        excelfile = fileToPandas(fileName, filesExtension)
+
     # change Reported Time type from objet to datetime and set it as index
     excelfile[configDict['indexul']] = pd.to_datetime(excelfile[configDict['indexul']])
     excelfile.set_index(configDict['indexul'], inplace=True)
@@ -118,7 +174,7 @@ for fileName in fileNames:
     excelfile = excelfile[::-1]
     figura = plt.figure();
     ax = figura.add_subplot(111)
-    colectieGraphs = graficCollection(figura)
+    colectieGraphs = GraficCollection(figura)
     colectieGraphs.extrage_gafice(excelfile, FilterParams, TargetColumns, UseTargetColumnsInLegend)
     for label, serie in colectieGraphs.grafice.items():
         x = serie.index
@@ -132,15 +188,14 @@ for fileName in fileNames:
     dateTimeFmt = mdates.DateFormatter('%D %H:%M')
     colectieGraphs.figura.axes[0].xaxis.set_major_locator(plt.MaxNLocator(45))
     colectieGraphs.figura.axes[0].xaxis.set_major_formatter(dateTimeFmt)
-    colectieGraphs.figura.axes[0].xaxis.set_tick_params(rotation = 90)
-    colectieGraphs.figura.legend(loc=9, mode='none', fontsize='small', ncol=5, labelspacing=0.05)
+    colectieGraphs.figura.axes[0].xaxis.set_tick_params(rotation=90)
+    #colectieGraphs.figura.legend(loc=9, mode='none', fontsize='small', ncol=5, labelspacing=0.05)
+    colectieGraphs.figura.legend(loc=9,ncol=5)
     colectieGraphs.figura.set_size_inches(9.99, 6.7)
     colectieGraphs.figura.tight_layout()
-    colectieGraphs.figura.subplots_adjust(top = 0.900)
+    colectieGraphs.figura.subplots_adjust(top = 0.800)
 
     # ----------
-
-
 
     tmpPictureName = configDict['Title'] + '.png'
     plt.savefig(tmpPicturePath + tmpPictureName)
